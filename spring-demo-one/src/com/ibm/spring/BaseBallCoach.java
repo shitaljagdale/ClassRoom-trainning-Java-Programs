c138bc4dd5fa09a530f7434dc5c31aabb13fc465
Shital B Jadhav <shitjad1@in.ibm.com>
2019-03-14 15:56:24 +0530

    Serialization And Deserilization add

--- spring-demo-one/src/com/ibm/spring/BaseBallCoach.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.spring;
-
-public class BaseBallCoach implements Coach {
-	@Override
-	public String getDailyWorkOut() {
-		return "Spend 30 min for daily workout";
-	}
-
-}
--- spring-demo-one/src/com/ibm/spring/Coach.java
+++
@@ -1,6 +0,0 @@
-package com.ibm.spring;
-
-public interface Coach {
-	public String getDailyWorkOut();
-
-}
